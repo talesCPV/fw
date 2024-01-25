fb1e8cc35f9547f91262eca88312f237fadf52cd
talesCPV <tales@flexibus.com.br>
2024-01-25 11:54:06 -0300

    by_script

---
+++ backend/sql.php
@@ -0,0 +1,10 @@
+<?php
+
+$query_db = array(
+    "0"  => 'CALL sp_login("x00","x01");',
+    "1"  => 'CALL sp_view_calendar("x00","x01","x02");',
+    "2"  => 'CALL sp_set_calendar("x00","x01","x02");',
+ 
+    );
+
+?>
