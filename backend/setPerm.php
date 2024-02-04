0ab832627aef10fafc6d44a11a1968dec3b0908f
talesCPV <tales@flexibus.com.br>
2024-02-03 21:12:10 -0300

    by_script

--- backend/setPerm.php
+++ backend/setPerm.php
@@ -1,26 +1,14 @@
 <?php   
 	if (IsSet($_POST["hash"])){
         $path = getcwd().'/../config/menu.json';
-        $file = $_POST["file"];
+        $json = $_POST["json"];
 
-        if (file_exists($path)) {   
-            
-/*            
-            $fp = fopen($path, "r");
-            $txt = "";
-            while (!feof ($fp)) {
-                $txt = $txt . fgets($fp,4096);
-            }
-            fclose($fp); 
-            $json = json_decode($txt); 
+        $menu = json_decode($json, true);
+        var_dump($menu['menu']);
 
-            if(!property_exists($json, $user)){
-                $json->$user = new class{};
-            }               
-            $json->$user->$field = $value;                                        
-            return file_put_contents($path, json_encode($json));
-*/            
-        }        
+        return file_put_contents($path, json_encode($menu));
+
+         
     }
-
+ 
 ?>
